39beabf38fca68a2f5a3c5ae9c1e7a373037ea24
sheepsbleat <adamypadan@gmail.com>
2020-07-31 15:06:49 +0100

    💄 UI: added dialog [unstable]

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import PaletteMetaForm from "./PaletteMetaForm";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -8,60 +10,53 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import randomName from "./utils/Name";
-import { withStyles } from "@material-ui/styles";
+
 const drawerWidth = 400;
-const styles = (theme) => (
-  {
-
-    appBar: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      height: "64px"
-    },
-    appBarShift: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-
-    },
-    menuButton: {
-      marginLeft: 12,
-      marginRight: 20
-    },
-    navBtns: {
-
-    },
-    root: {
-      display: "flex",
-      flexDirection: "row",
-
-    }
-  }
-)
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: "64px"
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20
+  },
+  navBtns: {}
+});
 
 class PaletteFormNav extends Component {
-  state = {
-    newPaletteName: randomName()
+  constructor(props) {
+    super(props);
+    this.state = { newPaletteName: "" };
+    this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount() {
-    ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
-      this.props.palettes.every(
-        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
-      )
-    );
-  }
-  handleChange = (evt) => {
+
+  handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
     });
   }
   render() {
-    const { classes, open } = this.props;
+    const { classes, open, palettes, handleSubmit } = this.props;
     return (
       <div className={classes.root}>
         <CssBaseline />
-
         <AppBar
           position='fixed'
           color='default'
@@ -79,35 +74,20 @@ class PaletteFormNav extends Component {
               <MenuIcon />
             </IconButton>
             <Typography variant='h6' color='inherit' noWrap>
-              Palette Creator
-    </Typography>
-
+              Create A Palette
+            </Typography>
           </Toolbar>
           <div className={classes.navBtns}>
-            <ValidatorForm onSubmit={() => this.props.handleSubmit(this.state.newPaletteName)}>
-              <TextValidator
-                label='Palette Name'
-                value={this.state.newPaletteName}
-                name='newPaletteName'
-                onChange={this.handleChange}
-                validators={["required", "isPaletteNameUnique"]}
-                errorMessages={["Enter Palette Name", "Name already used"]}
-              />
-              <Button variant='contained' color='primary' type='submit'>
-                Save Palette
-      </Button>
-
-
-            </ValidatorForm>
-            <Link to="/">
-              <Button variant="contained" color="secondary">
+            <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} />
+            <Link to='/'>
+              <Button variant='contained' color='secondary'>
                 Go Back
-                </Button>
+              </Button>
             </Link>
           </div>
         </AppBar>
       </div>
-    )
+    );
   }
 }
 export default withStyles(styles, { withTheme: true })(PaletteFormNav);
